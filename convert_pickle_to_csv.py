b8c194f6bd18b7b6bb6b73e041d915b7398ba3b6
srushs5699 <srushtishinde12@gmailcom>
2024-10-11 23:19:43 -0400

    comments for pickle to csv file

--- convert_pickle_to_csv.py
+++ convert_pickle_to_csv.py
@@ -1,29 +1,29 @@
-# import pandas as pd
-# import pickle
-
-# # Load the pickle file
-# with open('df_minutes.pickle', 'rb') as f:
-#     df = pickle.load(f)
-
-# # Save the DataFrame to a CSV file
-# df.to_csv('df_minutes.csv', index=True)
-
-# print("Pickle file successfully converted to CSV!")
-
-# import pandas as pd
-# import pickle
-
-# # Load the pickle file
-# with open('df_press_conferences.pickle', 'rb') as f:
-#     df = pickle.load(f)
-
-# # Save the DataFrame to a CSV file
-# df.to_csv('df_press_conferences.csv', index=True)
-
-# print("Pickle file successfully converted to CSV!")
+# code to convert df_minutes.pickle to df_minutes.csv
 
 import pandas as pd
 import pickle
+
+# Load the pickle file
+with open('df_minutes.pickle', 'rb') as f:
+    df = pickle.load(f)
+
+# Save the DataFrame to a CSV file
+df.to_csv('df_minutes.csv', index=True)
+
+print("Pickle file successfully converted to CSV!")
+
+# code to convert df_press_conferences.pickle to df_press_conferences.csv
+
+# # Load the pickle file
+with open('df_press_conferences.pickle', 'rb') as f:
+    df = pickle.load(f)
+
+# Save the DataFrame to a CSV file
+df.to_csv('df_press_conferences.csv', index=True)
+
+print("Pickle file successfully converted to CSV!")
+
+# code to convert all_fed_speeches.pickle to all_fed_speeches.csv
 
 # Load the pickle file
 with open('all_fed_speeches.pickle', 'rb') as f:
